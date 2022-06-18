2ffa66810e74fe7c57fa426fd358096c79476e02
Aeman Abdulmuhssen <60732388+WereWolfx01@users.noreply.github.com>
2022-06-18 01:34:22 -0400

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.jpeg";
-import editor from "../../Assets/Projects/codeEditor.png";
 import portfolio from "../../Assets/Projects/portfolio.png";
-import suicide from "../../Assets/Projects/suicide.png";
 import chatbox from "../../Assets/Projects/chatbox.gif";
 
 function Projects() {
